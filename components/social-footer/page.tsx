36c3c7e95c2194138544c0e6dda5ea402ec296c7
Samrat Ghimire <samratghimire32@gmail.com>
2025-06-29 11:08:02 +0545

    add resume

--- components/social-footer/page.tsx
+++ components/social-footer/page.tsx
@@ -1,6 +1,6 @@
 // import TwitterIcon from "@/icons/twitter";
 import TwitterIcon from "@/icons/twitter";
-import { Github, Linkedin } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
 
 const SocialFooter = () => {
@@ -16,12 +16,15 @@ const SocialFooter = () => {
             size={40}
           />
         </Link>
-        <Link href="https://x.com/Samrat0gh" target="_blank">
+        <Link href="https://x.com/samrat0gh" target="_blank">
           <TwitterIcon
             className="inline hover:text-cyan-400 py-2 px-2 w-[40px] h-[40px]"
             color="#fff"
           />
         </Link>
+        <a href="mailto:contact@samratghimire.tech">
+          <Mail className="inline hover:text-cyan-400 py-2 px-2" size={40} />
+        </a>
       </div>
     </div>
   );
